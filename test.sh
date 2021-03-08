8442aa82916f9f547bb9cf1e7cd73efde3ae5baf
Martin Skoviera <skoviera@skoviera-macbookpro2.roam.corp.google.com>
2021-03-08 10:00:07 +0100

    PROJECT_NUMBER

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Going to output PROJECT_ID"
 echo $PROJECT_ID
-echo "PROJECT_ID should be on the previous line"
+echo $PROJECT_NUMBER
+echo "PROJECT_ID and PROJECT_NUMBER should be on the previous line"
